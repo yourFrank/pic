0caa7fdd7fc6b244870135f28af3926ac3738d0c
yourFrank <43976830+yourFrank@users.noreply.github.com>
2021-08-21 08:17:07 +0800

    Delete my.js

--- style/my.js
+++
@@ -1,9 +0,0 @@
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-
-//页脚博主信息
-$(document).ready(function(e) {
-  $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 心语 | xinyu');
-});
